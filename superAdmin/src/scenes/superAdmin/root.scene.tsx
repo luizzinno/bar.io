e3ea348440169eefcca2258ee1a36c0f7252ddd4
Pedro Dominguez Aguilera <dominguezaguilera15@gmail.com>
2021-02-08 17:09:44 +0100

    add scaffolding super admin

---
+++ superAdmin/src/scenes/superAdmin/root.scene.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CenteredLayout } from 'layouts';
+
+export const RootScene: React.FC = () => {
+  return (
+    <>
+      <CenteredLayout>
+        <p>Hola super admin</p>
+      </CenteredLayout>
+    </>
+  );
+};
