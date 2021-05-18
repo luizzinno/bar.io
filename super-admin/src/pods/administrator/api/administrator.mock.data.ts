0b2da453370ee3bd2dd14dcbd735fc4074932f8a
manudous <you@example.com>
2021-05-18 18:37:01 +0200

    add validation to fields

--- super-admin/src/pods/administrator/api/administrator.mock.data.ts
+++ super-admin/src/pods/administrator/api/administrator.mock.data.ts
@@ -3,7 +3,7 @@ import { AdministratorInfo } from './administrator.api-model';
 
 export const mockAdministartorInfo: AdministratorInfo = {
   id: '1',
-  name: 'My restaurant',
+  name: 'Juan',
   email: 'test@gmail.com',
   numberPhone: '6271831947',
   initialPassword: 'Kjdasa12',
--- super-admin/src/pods/administrator/components/administrator-info/administrator-info.validation.ts
+++ super-admin/src/pods/administrator/components/administrator-info/administrator-info.validation.ts
@@ -3,8 +3,38 @@ import { createFormikValidation } from '@lemoncode/fonk-formik';
 
 const validationSchema: ValidationSchema = {
   field: {
-    name: [Validators.required],
-    email: [Validators.required, Validators.email],
+    name: [
+      {
+        validator: Validators.required,
+        message: 'Por favor complete este campo obligatorio',
+      },
+      {
+        validator: Validators.minLength,
+        customArgs: { length: 3 },
+        message: 'El valor proporcionado no cumple con la longitud mínima',
+      },
+    ],
+    email: [
+      {
+        validator: Validators.required,
+        message: 'Por favor complete este campo obligatorio',
+      },
+      {
+        validator: Validators.email,
+        message: 'Email no válido',
+      },
+    ],
+    numberPhone: [
+      {
+        validator: Validators.required,
+        message: 'Por favor complete este campo obligatorio',
+      },
+      {
+        validator: Validators.pattern,
+        customArgs: { pattern: /^[0-9]{2,3}-? ?[0-9]{6,7}$/ },
+        message: 'Proporcione un número de teléfono válido',
+      },
+    ],
   },
 };
 
