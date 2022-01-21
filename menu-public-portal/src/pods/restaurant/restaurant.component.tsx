4996484c3e8b2b0ed79ded3741bfd06ad66fd775
manudous <you@example.com>
2022-01-21 13:16:19 +0100

    emptying the crab

--- menu-public-portal/src/pods/restaurant/restaurant.component.tsx
+++ menu-public-portal/src/pods/restaurant/restaurant.component.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import * as classes from "./restaurant.styles";
-import { RestaurantInfo, Items, PriceByRation } from "./restaurant.vm";
+import {
+  RestaurantInfo,
+  Items,
+  PriceByRation,
+  CategoryEntry,
+} from "./restaurant.vm";
 
 interface PropsRation {
   ration: PriceByRation[];
 }
-
 const RationComponent: React.FC<PropsRation> = (props) => {
   const { ration } = props;
   return (
@@ -20,37 +24,63 @@ const RationComponent: React.FC<PropsRation> = (props) => {
   );
 };
 
+interface PropsItemComponent {
+  item: Items;
+}
+
+const ItemComponent: React.FC<PropsItemComponent> = (props) => {
+  const { item } = props;
+  return (
+    <li key={item.name}>
+      <p>{item.name}</p>
+      {item.description ? <p>Descripción: {item.description}</p> : null}
+      {item.price ? <p>Precio: {item.price} €</p> : null}
+      {item.priceByRation ? (
+        <ul>
+          <RationComponent ration={item.priceByRation} />
+        </ul>
+      ) : null}
+    </li>
+  );
+};
+
 interface PropsItemsComponent {
   items: Items[];
 }
 export const ItemsComponent: React.FC<PropsItemsComponent> = (props) => {
   const { items } = props;
-
   return (
     <>
       {items.map((item) => (
-        <li key={item.name}>
-          <p>{item.name}</p>
-          {item.description ? <p>Descripción: {item.description}</p> : null}
-          {item.price ? <p>Precio: {item.price} €</p> : null}
-          {item.priceByRation ? (
-            <ul>
-              <RationComponent ration={item.priceByRation} />
-            </ul>
-          ) : null}
-        </li>
+        <ItemComponent item={item} />
       ))}
     </>
   );
 };
 
-interface Props {
+interface CategoryProps {
+  category: CategoryEntry;
+}
+
+const CategoryComponent: React.FC<CategoryProps> = (props) => {
+  const { category } = props;
+  return (
+    <li key={category.name}>
+      <h3>{category.name}</h3>
+      <ul>
+        <ItemsComponent items={category.items} />
+      </ul>
+    </li>
+  );
+};
+
+interface RestaurantProps {
   restaurantName: string;
   restaurantMenuInfo: RestaurantInfo;
 }
 
-export const RestaurantComponent: React.FC<Props> = (props) => {
-  const { restaurantName, restaurantMenuInfo } = props;
+export const RestaurantComponent: React.FC<RestaurantProps> = (props) => {
+  const { restaurantMenuInfo } = props;
   const { name, heading1, heading2, menu } = restaurantMenuInfo;
 
   return (
@@ -59,13 +89,8 @@ export const RestaurantComponent: React.FC<Props> = (props) => {
       <h2>{heading1}</h2>
       <h2>{heading2}</h2>
       <ul>
-        {menu.map((item) => (
-          <li key={item.name}>
-            <h3>{item.name}</h3>
-            <ul>
-              <ItemsComponent items={item.items} />
-            </ul>
-          </li>
+        {menu.map((category) => (
+          <CategoryComponent category={category} />
         ))}
       </ul>
     </>
