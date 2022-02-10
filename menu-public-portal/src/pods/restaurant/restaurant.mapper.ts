cc9d491bd7b8f2bc7b71dee1a0e26f142a999102
Braulio Diez Botella <braulio.diez@gmail.com>
2022-02-10 11:56:56 +0100

    todo added

--- menu-public-portal/src/pods/restaurant/restaurant.mapper.ts
+++ menu-public-portal/src/pods/restaurant/restaurant.mapper.ts
@@ -13,6 +13,8 @@ const chooseTheme = (themeName: restaurantVm.ThemeName): Theme => {
   }
 };
 
+// TODO: Not in use now since we are directly using mock viewModel JSON
+// once we have a repo model we will apply this mock
 /*
 export const mapFromRestaurantApiToRestaurantVm = (
   restaurantApi: restaurantApi.RestaurantInfoGlobal
