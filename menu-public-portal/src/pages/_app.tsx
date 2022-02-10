a1a4991dce3f5b030ceb7e2f7c5d93a976f7fd09
Braulio Diez Botella <braulio.diez@gmail.com>
2022-02-10 11:12:40 +0100

    fish theme created

--- menu-public-portal/src/pages/_app.tsx
+++ menu-public-portal/src/pages/_app.tsx
@@ -4,11 +4,11 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "core/theme";
+import { theme, fishTheme } from "core/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={fishTheme}>
       <Component {...pageProps} />
     </ThemeProvider>
   );
