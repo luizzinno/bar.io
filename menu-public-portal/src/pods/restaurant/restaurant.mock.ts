e9157c66a4475f46abb9839da80ff3f8d52d6b97
Antonio  Contreras LEMONCODE <antonio.contreras@lemoncode.net>
2022-02-15 17:13:40 +0100

    add entrantes to new menu

--- menu-public-portal/src/pods/restaurant/restaurant.mock.ts
+++ menu-public-portal/src/pods/restaurant/restaurant.mock.ts
@@ -548,7 +548,84 @@ const hermanosAlbaFutureJSON: RestaurantInfo = {
   ],
 };
 
+const estacionTerminoFutureJSON: RestaurantInfo = {
+  name: "Estación Término",
+  urlName: "estacionTermino",
+  theme: "default",
+  phone: "+34952401096",
+  address: "Plaza del Señorío, Edificio Garcia Lorca, 8 29730 Málaga",
+  locationUrl:
+    "https://www.google.com/maps/place/Estaci%C3%B3n+T%C3%A9rmino+Restaurante/@36.7166232,-4.2816961,15z/data=!4m5!3m4!1s0x0:0xdfb3fd3416c42c60!8m2!3d36.7166232!4d-4.2816961",
+  description: "",
+  menu: [
+    {
+      name: "Entrantes",
+      items: [
+        {
+          name: "Sopa fría del día",
+          price: 5,
+        },
+        {
+          name: "Sopa caliente del día",
+          price: 5,
+        },
+        {
+          name: "Ensalada gratinada",
+          description: "Lechuga, Queso de Cabra, Nueces y Manzana",
+          price: 8,
+        },
+        {
+          name: "Ensalada tricolor",
+          description: "Tomate, Aguacate, Queso Mozarela",
+          price: 8,
+        },
+        {
+          name: "Ensalada tropical",
+          description: "Lechuga, Aguacate, Piña y Melocotón, Palmito",
+          price: 8,
+        },
+        {
+          name: "Ensalada E.T",
+          description: "Zanahoria, Manzana, Pasas, Maiz, Nueces",
+          price: 6.5,
+        },
+        {
+          name: "Ensalada Atapay",
+          description: "Lechuga, Apio, Manzana, Nueces y Pollo",
+          price: 8,
+        },
+        {
+          name: "Milhojas",
+          description: "Verduras a la plancha con Queso de Cabra",
+          price: 9,
+        },
+        {
+          name: "Croquetas Caseras",
+          price: 8,
+        },
+        {
+          name: "Camembert Frito",
+          price: 8,
+        },
+        {
+          name: "Pincho de Langostinos y Vieras",
+          price: 9.5,
+        },
+        {
+          name: "Langostinos Rebozados",
+          price: 9.5,
+        },
+        {
+          name: "Langostinos al Curry",
+          price: 10.25,
+        },
+      ],
+    },
+  ],
+};
+
 export const restaurantMockData: RestaurantInfo[] = [
   papulinosFutureJSON,
   hermanosAlbaFutureJSON,
+  estacionTerminoFutureJSON,
 ];
