081ded3faf3119e4e021786047d9e643b252a641
nasdan <daniel.sanchez@lemoncode.net>
2022-02-16 12:11:20 +0100

    add pod

---
+++ tools/src/pods/restaurant/restaurant.mappers.ts
@@ -0,0 +1,5 @@
+import * as apiModel from './restaurant.api-model'
+import * as model from 'dals'
+export const mapRestaurantFromApiToModel = (restaurant: apiModel.RestaurantInfoGlobal): model.Restaurant => {
+  
+}
